b2d1fdd2df19af6efaa6e93892994efb8aba3991
Sandro <sandrolima@gmail.com>
2017-12-15 05:21:30 -0200

    acrescentei vetor

--- teste.php
+++ teste.php
@@ -14,4 +14,8 @@ Verificar Acentuação
 phpinfo();
 echo "That's it !"
 echo "uma linha a mais... ççÇÇçç";
+$vet1=array("casa"=>"Modo antigo");
+$vet2=["casa":"não sei se funciona"];
+$vet3=["casa"=>"também nãoo sei se funciona"];
+
 ?>
